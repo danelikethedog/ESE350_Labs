3f54d4ebbd8977f9144734b023b0ebf76675152b
Dane Walton <dwalton@seas.upenn.edu>
2016-01-28 16:50:32 -0500

    Gen 420Hz Wave

--- ESE350_Lab03/main.c
+++ ESE350_Lab03/main.c
@@ -5,8 +5,38 @@
 #include "uart.h"
 
 
-ISR(TIMER1_)
+//OC1A used as PB1
+int HiOrLo, stallTime;
+ 
+
+ISR(TIMER1_COMPA_vect) {
+
+	OCR1A += stallTime;
+
+}
+
+int freqCalc(int inHertz) {
+	return (1/inHertz)*16*100000;
+}
 
 int main(void) {
+
+	stallTime = 18073;
+
+	DDRB |= 0xFF;
+
+	//PORTB |= 0x02;
+
+	TCCR1A = 0x40;
+
+	TCCR1B = 0x01;
+
+	TIMSK1 = 0x02;
+
+	OCR1A = TCNT1 + 16;
+
+	sei();
+
+	while(1);
 
 }
