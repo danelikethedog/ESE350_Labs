6854946cea6bdc1b4e8c6253f89a2b489fcb0e06
Dane Walton <dwalton@seas.upenn.edu>
2016-02-19 13:45:22 -0500

    Tutorial ADC

--- ESE350_Tutorials/ADC/main.c
+++ ESE350_Tutorials/ADC/main.c
@@ -4,17 +4,34 @@
 #include <stdio.h>
 #include "uart.h"
 
+
+volatile int sample = 0;
+
 int main(void) {
 
 
 
+	//Light Sensor Set Up
+	DDRC = 0x00;
+	ADMUX |= 0x40;
+	ADCSRA |= 0xE0;
+	ADCSRB = 0x00;
+
+	DDRB = 0x20;
+
 	sei();
 	uart_init();
-	adc_init();
 
 
 	while(1) {
+		sample = ADC;
+		if (sample < 512) {
+			PORTB = 0x00;
+		} else {
+			PORTB = 0x20;
+		}
 
+		printf("Value: %i\n", sample);
 	}
 
 }
