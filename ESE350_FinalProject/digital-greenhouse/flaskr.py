26f6532dada775c0c0539fed1bb35c3a0d0ce23d
williamrchr <williamrchr@gmail.com>
2016-04-21 00:10:52 -0400

    ui additions and updates to db design

--- ESE350_FinalProject/digital-greenhouse/flaskr.py
+++ ESE350_FinalProject/digital-greenhouse/flaskr.py
@@ -75,11 +75,15 @@ def show_entries():
     entries = cur.fetchall()
     return render_template('show_entries.html', entries=entries)
 
+@app.route('/test')
+def html_dev():
+	db = get_db()
+	cur = db.execute('select plant from entries')
+	plants = cur.fetchall()
+	return render_template('index-new.html', entries=plants)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
     db = get_db()
     db.execute('insert into entries (plant, temperature, water, light_intensity) values (?, ?, ?, ?)',
                [request.form['plant'], request.form['temperature'], request.form['water'], request.form['light_intensity']])
@@ -87,27 +91,5 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
-    return render_template('login.html', error=error)
-
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect(url_for('show_entries'))
-	
 if __name__ == '__main__':
 	app.run()
--- ESE350_FinalProject/digital-greenhouse/static/index-script.js
+++ ESE350_FinalProject/digital-greenhouse/static/index-script.js
@@ -1,6 +1,18 @@
  $(document).ready(function(e) {   
     // your code here
-	$('.clickable').click(function() {
-	$(this).children('.hid').toggle();
+	var mouseX;
+	var mouseY;
+	$('.clickable').click(function(e) {
+		if($('.dropdown').css('display') == 'none' && e.target == this) {
+		mouseX = e.pageX;
+		mouseY = e.pageY;
+		console.log('showing');
+		$('.dropdown').css({'top': mouseY-70, 'left':mouseX-465, 'position':'absolute'}).show();
+		}}); 
+		
+		
+	$('.exit').click(function() {
+		$('.dropdown').hide();
+		console.log('hidden');
 	});
  });
